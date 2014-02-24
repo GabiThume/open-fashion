f576ec95ae6e3728d7cc8c40fa86af29248888db
Vilson Vieira <vilson@void.cc>
2014-02-24 01:44:56 -0300

    add length and angle between lines calc

--- js/open-fashion.js
+++ js/open-fashion.js
@@ -1,17 +1,54 @@
-window.onload = function() {
-  var Vector = function (x, y) {
+window.onload = function () {
+  var Point = function (x, y) {
     this.x = x;
     this.y = y;
     this.coords = [x, y];
   };
-  
-  // Polygon sides
-  var vectors = [new Vector(100, 10),
-                 new Vector(100, 400),
-                 new Vector(300, 400),
-                 new Vector(250, 130),
-                 new Vector(300, 10)];
 
+  var Line = function (from, to) {
+    this.from = from;
+    this.to = to;
+  };
+  Line.prototype.length = function () {
+    return Math.sqrt(Math.pow(this.to.x-this.from.x, 2) +
+                     Math.pow(this.to.y-this.from.y, 2));
+  };
+  Line.prototype.angleWith = function (other, degree) {
+    var dx_this = this.to.x-this.from.x;
+    var dy_this = this.to.y-this.from.y;
+    var dx_other = other.to.x-other.from.x;
+    var dy_other = other.to.y-other.from.y;
+
+    // Dot product of the two vectors formed by the two lines
+    var dot = dx_this*dx_other + dy_this*dy_other;
+    // Product of the squared lengths
+    var squared = (dx_this*dx_this+dy_this*dy_this) *
+      (dx_other*dx_other+dy_other*dy_other);
+
+    var angle_rad = Math.acos(dot/Math.sqrt(squared));
+
+    if (degree) {
+      return angle_rad * 180 / Math.PI;
+    }
+    return angle_rad;
+  };
+  
+  // Polygon points
+  var points = [new Point(100, 10),
+                new Point(100, 400),
+                new Point(300, 400),
+                new Point(250, 130),
+                new Point(300, 10)];
+
+  // Polygon sides
+  var sides = [];
+  for (var i=0; i<points.length-1; i++) {
+    sides.push(new Line(points[i], points[i+1]))
+  }
+  sides.push(new Line(points[points.length-1], points[0]));
+  
+  console.log(sides[0].angleWith(sides[1], true));
+  
   // Paperjs setup
   var canvas = document.getElementById('canvas');
   paper.setup(canvas);
@@ -20,18 +57,18 @@ window.onload = function() {
   var path = new paper.Path();
   path.strokeColor = 'black';
 
-  for (var i in vectors) {
-    var coords = vectors[i].coords;
-    path.add(new paper.Point(coords));
+  for (var i in points) {
+    path.add(new paper.Point(points[i].coords));
   }
-  var coords = vectors[0].coords;
-  path.add(coords);
+  path.add(points[0].coords);
 
   // Draws
   paper.view.draw();
 
   // Cassowary solver setup
   var solver = new c.SimplexSolver();
+
+  var waist = sides[4].length();
 
   // Testing a simple linear system: 
   //    2x + 3y =  8      roots:  x = 1, y = 2 
